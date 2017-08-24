e41ed5256f0c70e22cd7e9c61c2cb3b45271b3a1
181192 <181192@stud.hvl.no>
2017-08-24 17:00:35 +0200

    Added Javadoc and Exceptions

--- Lab2_Oppgave6/src/no/hvl/dat104/TempOmregner.java
+++ Lab2_Oppgave6/src/no/hvl/dat104/TempOmregner.java
@@ -3,11 +3,23 @@
  */
 package no.hvl.dat104;
 
+import java.text.DecimalFormat;
+
 /**
- * @author krist
+ * @author Kristoffer-Andre Kalliainen
  *
  */
 public class TempOmregner {
+
+	/**
+	 * Skjekker input for temperatur
+	 * 
+	 * @param temp
+	 *            Temperatur som en streng
+	 * @return Temperaturen som en Double
+	 * @throws IllegalArgumentException
+	 *             Kaster exception hvis strenger er tom eller null.
+	 */
 	public Double skjekkInputTemperatur(String temp) throws IllegalArgumentException {
 		Double t = 0.0;
 		if (temp == null || temp == "") {
@@ -23,23 +35,60 @@ public class TempOmregner {
 		return t;
 	}
 
-	public boolean skjekkInputEnhet(String enhet) {
-		return enhet != null;
-	}
-
-	public double regnTemperatur(Double temp, String enhet) {
-		if (enhet.equals("celsius")) {
-			return fraCelsiusTilFahrenheit(temp);
-		} else {
-			return fraFahrenheitTilCelsius(temp);
+	/**
+	 * Skjekker input for temperatur enhet
+	 * 
+	 * @param enhet
+	 *            Temperatur enhet
+	 * @return Lovlig temperatur type
+	 * @throws IllegalArgumentException
+	 *             Kaster exception hvis strengen er tom eller null.
+	 */
+	public String skjekkInputEnhet(String enhet) throws IllegalArgumentException {
+		if (enhet == null || enhet == "") {
+			throw new IllegalArgumentException("Enhet input er tom");
 		}
+
+		return enhet;
 	}
 
-	private double fraCelsiusTilFahrenheit(double celsius) {
+	/**
+	 * Regner ut temperaturen, ut ifra hvilken type temperatur enhet som gies.
+	 * 
+	 * @param temp
+	 *            Temperaturen som skal omregnes
+	 * @param enhet
+	 *            Temperatur enheten temperaturen oppgies i
+	 * @return Temperaturen omregnet til enten Celsius eller Fahrenheit
+	 */
+	public String regnTemperatur(Double temp, String enhet) {
+		DecimalFormat df = new DecimalFormat("####0.00");
+		Double svar;
+		if (enhet.equals("celsius")) {
+			svar = fraCelsiusTilFahrenheit(temp);
+		} else {
+			svar = fraFahrenheitTilCelsius(temp);
+		}
+		return df.format(svar).replace(",", ".");
+	}
+
+	/**
+	 * Hjelpemetode for å konventere fra celsius til fahrenheit
+	 * 
+	 * @param celsius
+	 * @return fahrenheit
+	 */
+	private Double fraCelsiusTilFahrenheit(Double celsius) {
 		return ((celsius * 1.8) + 32);
 	}
 
-	private double fraFahrenheitTilCelsius(double fahrenheit) {
+	/**
+	 * Hjelpemetode for å konventere fra fahrenheit til celsius
+	 * 
+	 * @param fahrenheit
+	 * @return celsius
+	 */
+	private Double fraFahrenheitTilCelsius(Double fahrenheit) {
 		return ((fahrenheit - 32) / 1.8);
 	}
 }
