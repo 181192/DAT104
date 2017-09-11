0dc279520018ba862ded096b0eee1209a0944e8b
Kristoffer-Andre Kalliainen <181192@stud.hvl.no>
2017-09-11 18:05:14 +0200

    Added VareDAOTest methods
    Signed-off-by: 181192 <181192@stud.hvl.no>

---
+++ Lab4_Oppgave1/test/no/hvl/dat104/dataaccess/VareDAOTest.java
@@ -0,0 +1,31 @@
+package no.hvl.dat104.dataaccess;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class VareDAOTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void leggTilVare() {
+    }
+
+    @Test
+    void finnVare() {
+    }
+
+    @Test
+    void oppdaterVare() {
+    }
+
+    @Test
+    void slettVare() {
+    }
+
+    @Test
+    void alleVarer() {
+    }
+
+}
