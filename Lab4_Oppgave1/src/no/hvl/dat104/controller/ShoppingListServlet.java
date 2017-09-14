fca9e2ed1c0051f09d970fcbbcfbb1364687c512
Kristoffer-Andre Kalliainen <181192@stud.hvl.no>
2017-09-15 00:20:14 +0200

    Added url mappings and flash/validations utils
    Signed-off-by: 181192 <181192@stud.hvl.no>

--- Lab4_Oppgave1/src/no/hvl/dat104/controller/ShoppingListServlet.java
+++ Lab4_Oppgave1/src/no/hvl/dat104/controller/ShoppingListServlet.java
@@ -1,55 +1,71 @@
 package no.hvl.dat104.controller;
 
-import no.hvl.dat104.Validator;
 import no.hvl.dat104.dataaccess.IItemEAO;
-import no.hvl.dat104.dataaccess.jpa.ItemEAO;
 import no.hvl.dat104.model.ItemEntity;
+import no.hvl.dat104.util.FlashUtil;
+import no.hvl.dat104.util.ValidatorUtil;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+import static no.hvl.dat104.controller.UrlMappings.LOGIN_URL;
 import static no.hvl.dat104.controller.UrlMappings.SHOPPING_LIST_URL;
 
 @WebServlet(name = "ShoppingListServlet", urlPatterns = "/" + SHOPPING_LIST_URL)
-public class ShoppingListServlet extends javax.servlet.http.HttpServlet {
+public class ShoppingListServlet extends HttpServlet {
     @EJB
     private IItemEAO itemEAO;
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String submitType = Validator.escapeHtml(request.getParameter("submit"));
-        String item = Validator.escapeHtml(request.getParameter("item"));
-        System.out.println(submitType);
-        System.out.println(item);
-        if (Validator.isValidString(submitType) && Validator.isValidSubmitType(submitType) && Validator.isValidString(item)) {
-            if (submitType.equals("Slett")) {
-                itemEAO.removeItem(itemEAO.findItem(item));
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if (request.getSession().getAttribute("loggedInUser") != null) {
+            String submitType = ValidatorUtil.escapeHtml(request.getParameter("submit"));
+
+            if (ValidatorUtil.isValidString(submitType) && ValidatorUtil.isValidSubmitType(submitType)) {
+                if (submitType.equals("Slett")) {
+                    deleteItem(request);
+                    FlashUtil.flash(request, "Success", "Vare slettet");
+                } else {
+                    newItem(request);
+                    FlashUtil.flash(request, "Success", "Vare lagt til");
+                }
             } else {
-                itemEAO.addItem(new ItemEntity(item));
+                FlashUtil.flash(request, "Error", "Ugyldig input");
             }
-            response.sendRedirect("shoppinglist");
-        } else {
-            response.sendError(500, "Validering av input feilet");
         }
+        response.sendRedirect(SHOPPING_LIST_URL);
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<ItemEntity> shoppingList = itemEAO.allItems();
-        request.setAttribute("shoppingList", shoppingList);
-        response.setContentType("text/html; charset=ISO-8859-1");
-        try {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if (request.getSession().getAttribute("loggedInUser") != null) {
+            List<ItemEntity> shoppingList = itemEAO.allItems();
+            request.setAttribute("shoppingList", shoppingList);
             request.getRequestDispatcher("WEB-INF/shoppingList.jsp").forward(request, response);
-        } catch (ServletException e) {
-            e.printStackTrace();
+        } else {
+            FlashUtil.needToBeLoggedIn(request);
+            response.sendRedirect(LOGIN_URL);
         }
     }
 
-    @Override
-    public void init() throws ServletException {
-        itemEAO = new ItemEAO();
+    private void deleteItem(HttpServletRequest request) {
+        String delItem = ValidatorUtil.escapeHtml(request.getParameter("delItem"));
+        if (ValidatorUtil.isValidNumber(delItem)) {
+            ItemEntity item = itemEAO.findItem(Integer.parseInt(delItem));
+            itemEAO.removeItem(item);
+        }
+    }
+
+    private void newItem(HttpServletRequest request) {
+        String itemName = ValidatorUtil.escapeHtml(request.getParameter("newItem"));
+        if (ValidatorUtil.isValidString(itemName)) {
+            itemEAO.addItem(new ItemEntity(itemName));
+        }
     }
 }
---
+++ Lab4_Oppgave1/src/no/hvl/dat104/util/InnloggingUtil.java
@@ -0,0 +1,41 @@
+package no.hvl.dat104.util;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class InnloggingUtil {
+
+    public static boolean isGyldigBrukernavn(String brukernavn) {
+        return brukernavn != null;
+    }
+
+    public static boolean isInnlogget(HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
+        return (session != null)
+                && (session.getAttribute("loggedInUser") != null);
+    }
+
+    public static String isInnloggetSom(HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
+        return isInnlogget(request) ?
+                (String) session.getAttribute("loggedInUser") : null;
+    }
+
+    public static void loggInnSom(HttpServletRequest request, String brukernavn) {
+
+        loggUt(request);
+        HttpSession sesjon = request.getSession(true);
+        sesjon.setAttribute("loggedInUser", brukernavn);
+    }
+
+    public static void loggUt(HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+    }
+
+}
