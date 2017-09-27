2732fedbda9b08da5f9653bfe769cf6afb6690bb
181192 <181192@stud.hvl.no>
2017-09-27 23:45:47 +0200

    typo

--- Oblig02/sql/seedDB.sql
+++ Oblig02/sql/seedDB.sql
@@ -7,13 +7,13 @@ CREATE TABLE fest.deltager(
 	mobil 			INTEGER 	NOT NULL,
 	fornavn 		VARCHAR		NOT NULL,
 	etternavn 		VARCHAR		NOT NULL,
-	is_mann			BOOLEAN 	NOT NULL,
+	er_mann			BOOLEAN 	NOT NULL,
 	har_betalt 		BOOLEAN		NOT NULL,
 	er_kasserer		BOOLEAN		NOT NULL,
 	PRIMARY KEY(mobil)
 );
 
-INSERT INTO fest.deltager (mobil, fornavn, etternavn, is_mann, betalingsstatus) VALUES
-(45656778, 'Arne', 'Arnesen', true, null),
-(90123532, 'Peder', 'Pedersen', true, false),
-(54322113, 'Kari', 'Karisen', false, true);
+INSERT INTO fest.deltager (mobil, fornavn, etternavn, er_mann, har_betalt, er_kasserer) VALUES
+(45656778, 'Arne', 'Arnesen', true, true, true),
+(90123532, 'Peder', 'Pedersen', true, false, false),
+(54322113, 'Kari', 'Karisen', false, true, false);
