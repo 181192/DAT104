fee811a1d78c1952067ecf1118ef9d0a9d4310ec
181192 <181192@stud.hvl.no>
2017-09-27 15:00:38 +0200

    Added jsp pages

--- Oblig02/src/no/hvl/dat104/controller/kasserer/BetalingsOversiktServlet.java
+++ Oblig02/src/no/hvl/dat104/controller/kasserer/BetalingsOversiktServlet.java
@@ -15,8 +15,7 @@ public class BetalingsOversiktServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("WEB-INF/jsp/kasserer/betalingsoversikt.jsp").forward(request, response);;
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
--- Oblig02/src/no/hvl/dat104/controller/kasserer/KassererLoginServlet.java
+++ Oblig02/src/no/hvl/dat104/controller/kasserer/KassererLoginServlet.java
@@ -15,8 +15,7 @@ public class KassererLoginServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("WEB-INF/jsp/kasserer/kassererlogin.jsp").forward(request, response);;
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
