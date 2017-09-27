dafc7f12cdf6e1929b03c2b1a78f8596752d8cb8
181192 <181192@stud.hvl.no>
2017-09-27 20:06:32 +0200

    Typo

--- Oblig02/src/no/hvl/dat104/controller/UrlMappings.java
+++ Oblig02/src/no/hvl/dat104/controller/UrlMappings.java
@@ -9,10 +9,10 @@ package no.hvl.dat104.controller;
  */
 public class UrlMappings {
 	public static final String BETALINGSOVERSIKT_URL = "betalingsoversikt";
-	public static final String DELTAGERELISTE_URL = "deltagere";
+	public static final String DELTAGERLISTE_URL = "deltagere";
 	public static final String FERDIG_URL = "ferdig";
 	public static final String KASSERERLOGIN_URL = "kassererlogin";
 	public static final String MOBILLOGIN_URL = "mobillogin";
 	public static final String PAAMELDING_URL = "paamelding";
-	public static final String PAAMELDINGBESKRIVELSE_URL = "paameldingbeskrivelse";
+	public static final String PAAMELDINGSBEKREFTELSE_URL = "paameldingsbekreftelse";
 }
