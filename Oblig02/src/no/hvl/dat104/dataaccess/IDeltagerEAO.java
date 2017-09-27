a9a21688b12f53c0c04eb38ea5d68e137da05f5a
181192 <181192@stud.hvl.no>
2017-09-27 20:07:38 +0200

    Added javadoc

--- Oblig02/src/no/hvl/dat104/dataaccess/IDeltagerEAO.java
+++ Oblig02/src/no/hvl/dat104/dataaccess/IDeltagerEAO.java
@@ -12,14 +12,44 @@ import no.hvl.dat104.model.DeltagerEntity;
  *
  */
 public interface IDeltagerEAO {
+	/**
+	 * Legger til en deltager
+	 * 
+	 * @param d
+	 *            Deltager
+	 */
 	public void leggTilDeltager(DeltagerEntity d);
 
+	/**
+	 * Finner en deltager basert på id, som er nummeret til deltageren
+	 * 
+	 * @param id
+	 *            nummeret
+	 * @return
+	 */
 	public DeltagerEntity finnDeltager(Integer id);
 
+	/**
+	 * Oppdaterer en deltager
+	 * 
+	 * @param d
+	 *            Deltager
+	 */
 	public void oppdaterDeltager(DeltagerEntity d);
 
+	/**
+	 * Sletter en deltager
+	 * 
+	 * @param d
+	 *            Deltager
+	 */
 	public void slettDeltager(DeltagerEntity d);
 
+	/**
+	 * Returnerer en liste med alle deltagerne
+	 * 
+	 * @return Alle deltagerne
+	 */
 	public List<DeltagerEntity> alleDeltagere();
 
 }
