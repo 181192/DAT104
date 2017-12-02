a7db89f295010e89e7657c3b2d2f0ab5bbe13184
181192 <181192@stud.hvl.no>
2017-12-02 17:15:46 +0100

    RegistrerServlet

--- SimpleLogin/src/main/java/no/kalli/controller/RegistrerServlet.java
+++ SimpleLogin/src/main/java/no/kalli/controller/RegistrerServlet.java
@@ -1,13 +1,48 @@
 package no.kalli.controller;
 
+import no.kalli.JspMapping;
+import no.kalli.UrlMapping;
+import no.kalli.dataaccess.StudentEAO;
+import no.kalli.model.Student;
+import no.kalli.util.FlashUtil;
+import no.kalli.util.PassordUtil;
+
+import javax.ejb.EJB;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 public class RegistrerServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    @EJB
+    private StudentEAO studentEAO;
 
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        RegistrerStudentValidator skjema = new RegistrerStudentValidator(request);
+        if (skjema.erAlleDataGyldig()) {
+            Student s = new Student();
+            s.setNavn(skjema.getNavn());
+            setOppSalt(s);
+            String passord = PassordUtil.hashPassord(skjema.getPassord(), s.getSalt());
+            s.setPassord(passord);
+            studentEAO.leggTilStudent(s, skjema.getKlasseKode());
+        } else {
+            skjema.settOppFeilmeldinger(request);
+            FlashUtil.Flash(request, "error", "Ugyldig input");
+            request.getSession().setAttribute("registrerStudentSkjema", skjema);
+            response.sendRedirect(UrlMapping.REGISTRER_URL);
+        }
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        request.getRequestDispatcher(JspMapping.REGISTRER_JSP).forward(request, response);
+    }
 
+    public void setOppSalt(Student s) {
+        try {
+            byte[] salt = PassordUtil.getSalt();
+            s.setSalt(Arrays.toString(salt));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
     }
 }
