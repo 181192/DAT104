d60593a5989985dbd3124c36f0de8ccbb440a0cb
Kn <kathinkane@hotmail.com>
2017-12-02 16:43:29 +0100

    Added Login JSP

--- SimpleLogin/src/main/java/no/kalli/controller/LoginServlet.java
+++ SimpleLogin/src/main/java/no/kalli/controller/LoginServlet.java
@@ -1,13 +1,19 @@
 package no.kalli.controller;
 
+import no.kalli.JspMapping;
+
 import java.io.IOException;
 
 public class LoginServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
+            throws javax.servlet.ServletException, IOException {
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
+            throws javax.servlet.ServletException, IOException {
+
+        request.getRequestDispatcher(JspMapping.LOGIN_JSP).forward(request, response);
 
     }
 }
--- SimpleLogin/web/WEB-INF/pages/login.jsp
+++ SimpleLogin/web/WEB-INF/pages/login.jsp
@@ -1,4 +1,4 @@
-<%--
+<%@ page import="static no.kalli.UrlMapping.LOGIN_URL" %><%--
   Created by IntelliJ IDEA.
   User: krist
   Date: 02.12.2017
@@ -12,7 +12,31 @@
 </head>
 <body>
 
-<form action=""></form>
+<form action="<%=LOGIN_URL%>" method="post">
+
+    <div class="imgcontainer">
+        <img src="img_avatar2.png" alt="Avatar" class="avatar">
+    </div>
+
+    <div class="container">
+        <label><b>Username</b></label>
+        <input type="text" placeholder="Enter Username" name="uname" required>
+
+        <label><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="psw" required>
+
+        <button type="submit">Login</button>
+        <input type="checkbox" checked="checked"> Remember me
+    </div>
+
+    <div class="container" style="background-color:#f1f1f1">
+        <button type="button" class="cancelbtn">Cancel</button>
+        <span class="psw">Forgot <a href="#">password?</a></span>
+    </div>
+
+
+
+</form>
 
 </body>
 </html>
